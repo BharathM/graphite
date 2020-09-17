b0cb7a53983c68a02e7216c26834a87c965e28d6
root <root@ubuntuu1604-AppWorks-Container.lab.opentext.com>
2020-09-17 12:38:27 +0530

    initial

---
+++ iostat_graphite.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import re
+import sys
+import time
+import socket
+import platform
+import subprocess
+import pickle
+import struct
+
+CARBON_SERVER = '10.96.152.46'
+CARBON_PICKLE_PORT = 2004
+DELAY = 0
+
+def get_iostat():
+    command = "iostat -xdy 10 1"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    stdout = process.communicate()[0].strip()
+    #Error logic to check iostat exists and data showing
+    postString = stdout.split("\n",2)[2]
+    lines = re.split('\n',postString)
+    iostat_data=[]
+    for line in lines:
+        iostat_data.append(line.split())
+    iostat_data[0] = [w.replace('/', '_') for w in iostat_data[0]]
+    iostat_data[0] = [w.replace('%', 'percent_') for w in iostat_data[0]]
+    print iostat_data
+    return iostat_data
+
+def run(sock, delay, hostname):
+    while True:
+        tuples = ([])
+        lines = []
+        iostat_data = get_iostat()
+        now = int(time.time())
+        r=len(iostat_data)
+        c=len(iostat_data[0])
+        print r
+        print c
+        for x in range(1,r):
+            for y in range(1,c):
+                print "%s-%s" % (x,y)
+                tuples.append((hostname+'.iostat.'+iostat_data[x][0]+'.'+iostat_data[0][y], (now,iostat_data[x][y])))
+        print("sending message")
+        package = pickle.dumps(tuples, 1)
+        size = struct.pack('!L', len(package))
+        sock.sendall(size)
+        sock.sendall(package)
+        time.sleep(delay)
+
+def main():
+    """Wrap it all up together"""
+    delay = DELAY
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if arg.isdigit():
+            delay = int(arg)
+        else:
+            sys.stderr.write("Ignoring non-integer argument. Using default: %ss\n" % delay)
+            
+    command1 = "hostname"
+    process1 = subprocess.Popen(command1, stdout=subprocess.PIPE, shell=True)
+    stdout1 = process1.communicate()[0].strip()
+    hostname=stdout1.replace('.', '-')
+    print hostname
+
+    sock = socket.socket()
+    try:
+        sock.connect( (CARBON_SERVER, CARBON_PICKLE_PORT) )
+    except socket.error:
+        raise SystemExit("Couldn't connect to %(server)s on port %(port)d, is carbon-cache.py running?" % { 'server':CARBON_SERVER, 'port':CARBON_PICKLE_PORT })
+
+    try:
+        run(sock, delay, hostname)
+    except KeyboardInterrupt:
+        sys.stderr.write("\nExiting on CTRL-c\n")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
